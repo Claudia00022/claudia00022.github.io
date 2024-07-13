22c5186ca8e2b804356154966d78d76832b9343d
Claudia00022 <klaudiaforysiak@gmail.com>
2024-07-13 13:13:09 +0100

    animated nav

--- src/components/nav/index.jsx
+++ src/components/nav/index.jsx
@@ -1,37 +1,63 @@
-import React from "react";
+import {React, useState} from "react";
 import { Link } from "react-router-dom";
 import './nav.style.css'
 
 
 const data = [
-  {
+  { id:0,
     label: "HOME",
     to: "/",
   },
-  {
+  { id:1,
     label: "ABOUT",
     to: "/about",
   },
-  {
+  { id:2,
     label: "WORK",
     to: "/work",
   },
-  {
+  { id:3,
     label: "ART",
     to: "/art",
   },
-  {
+  { id:4,
     label: "CONTACT",
     to: "/contact",
   },
 ];
 
 const NavBar = () => {
+  
+const [toogledLinks, setToogledLinks] = useState([]);
+
+function handleClick(id){
+if(toogledLinks.includes(id)){
+  setToogledLinks(toogledLinks.filter(linkId => linkId !== id));
+}else{
+  setToogledLinks([...toogledLinks, id]);
+}
+}
+
+
   return (
     <div className= 'absolute z-20 w-1/6 mt-64 ms-32'>
       <ul>
-       {data.map((item, key) => (
-        <li key={key}  ><Link to={item.to} className = 'flex flex-row items-center main-font mt-2'><div style={{width: '5px', height: '5px', backgroundColor: 'black', display: 'inline-block'}} className= 'me-2' ></div>{item.label}</Link></li>
+
+       {data.map((link) => (
+        <li key = {link.id} >
+        {toogledLinks.includes(link.id) ? ( <div 
+        onClick={() => handleClick(link.id)}
+          style={{width: '5px', height: '5px', backgroundColor: 'black', display: 'inline-block'}}
+           className= 'me-2' >
+          </div>) : ( <Link to={link.to}
+         onClick={()=> handleClick(link.id)}
+          className = 'flex flex-row items-center main-font mt-2'>
+         
+          {link.label}
+          </Link> )}
+
+         
+          </li>
        )) 
         
        }
--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,9 +1,59 @@
+import React, { useState } from 'react';
+
 function About() {
+
+  // Initialize state with an empty array
+  const [toggledLinks, setToggledLinks] = useState("home");
+  const [toggledAbout, setToggledAbout] = useState("about");
+  const [toggledContact, setToggledContact] = useState("contact")
+
+  function handleClick(){
+    setToggledLinks(
+     <div 
+
+          style={{width: '5px', height: '5px', backgroundColor: 'black', display: 'inline-block'}}
+           className= 'me-2' >
+          </div>
+    );
+    setToggledAbout("about");
+    setToggledContact("contact");
+  }
+
+  function handleAbout(){
+    setToggledAbout(<div 
+
+      style={{width: '5px', height: '5px', backgroundColor: 'black', display: 'inline-block'}}
+       className= 'me-2' >
+      </div>);
+      setToggledLinks("home");
+      setToggledContact("contact");
+  }
+
+  function handleContact(){
+    setToggledContact(<div 
+
+      style={{width: '5px', height: '5px', backgroundColor: 'black', display: 'inline-block'}}
+       className= 'me-2' >
+      </div>);
+      setToggledAbout("about");
+      setToggledLinks("home");
+  }
+
+
   return (
-    <>
-      <div>About</div>
-    </>
+<ul className='ms-12'>
+  <li onClick={handleClick}>
+    {toggledLinks}
+  </li>
+  <li onClick={handleAbout}>
+    {toggledAbout}
+  </li>
+  <li onClick={handleContact}>
+    {toggledContact}
+  </li>
+</ul>
   );
 }
+
 
 export default About;
