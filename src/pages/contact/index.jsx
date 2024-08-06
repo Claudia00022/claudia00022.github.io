26125f1f21eb4aa4e9062eca1be4c1a09804f036
Claudia00022 <klaudiaforysiak@gmail.com>
2024-08-06 17:05:24 +0100

    contact page

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -1,8 +1,20 @@
 import React from "react";
+import {Wave} from 'react-animated-text';
+import './contact.style.css';
 
 const Contact = () =>{
     return(
-        <div>Contact page</div>
+        <div className='rectangle contact'>
+         <button className=" contact_button"><a className='title'><Wave 
+            text = 'email'
+            effect = 'fadeOut'
+            speed = {4}
+            effectChange = {1.0}
+         /></a>
+         </button>
+         <button className=" contact_button"><a className='title'>instagram</a></button>
+         <button className=" contact_button"><a className='title'>linkedIn</a></button>
+    </div>
     )
 };
 
