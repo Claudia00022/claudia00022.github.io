b1ec36f25cbf1eac07a3db62ed5d810bd6c2b184
Claudia00022 <klaudiaforysiak@gmail.com>
2024-08-25 19:38:33 +0100

    PopUp

---
+++ src/components/popUp/PopUp.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+import './PopUp.style.css'
+
+
+function PopUp(){
+    const[close, setClose] = useState(false);
+    const [block, setBlock] = useState('block')
+
+    function handleBlock(){
+        setBlock('none')
+    }
+    
+    function handlePopUp(){
+      setClose(false);
+      console.log('hi')
+    }
+
+  return(
+    <div className='popUp_container' style={{display: block}}>
+        <button onClick={handleBlock}>close</button>
+    </div>
+  )
+}
+
+export default PopUp;
--- src/pages/art/index.jsx
+++ src/pages/art/index.jsx
@@ -1,59 +1,67 @@
 import React from "react";
-import PhotoOne from  '../../assets/photos/marlyn.JPG';
-import PhotoTwo from '../../assets/photos/girl.JPG';
-import PhotoThree from '../../assets/photos/women.JPG';
-import PhotoFour from '../../assets/photos/elephant.JPG';
-import PhotoFive from '../../assets/photos/womenInColor.JPG';
-import PhotoSix from '../../assets/photos/man.JPG';
+import PhotoOne from "../../assets/photos/marlyn.JPG";
+import PhotoTwo from "../../assets/photos/girl.JPG";
+import PhotoThree from "../../assets/photos/women.JPG";
+import PhotoFour from "../../assets/photos/elephant.JPG";
+import PhotoFive from "../../assets/photos/womenInColor.JPG";
+import PhotoSix from "../../assets/photos/man.JPG";
+import PhotoSeven from "../../assets/photos/three.jpg";
 
-import './art.style.css';
+import PopUp from "../../components/popUp/PopUp";
 
+import "./art.style.css";
 
+const arts_data = [
+  {
+    id: 1,
+    img_link: PhotoOne,
+  },
+  {
+    id: 2,
+    img_link: PhotoTwo,
+  },
+  {
+    id: 3,
+    img_link: PhotoThree,
+  },
+  {
+    id: 4,
+    img_link: PhotoFour,
+  },
+  {
+    id: 5,
+    img_link: PhotoFive,
+  },
+  {
+    id: 6,
+    img_link: PhotoSix,
+  },
+  {
+   id:7,
+   img_link: PhotoSeven
+  }
+];
 
-const Art = () =>{
-    return(
-      <>
-         <div className="right_bottom_rectangle">
-         <div className="columns-1 gap-5 sm:columns-2 sm:gap-8 md:columns-3 lg:columns-3 [&>img:not(:first-child)]:mt-8 ">
-
-     
-  
-          <img  src={PhotoOne}  className="object-contain"  ></img>
-  
-       
-      
-
-          
-          <img  src={PhotoTwo}  className="object-contain "></img>
-     
-
-     
+const Art = () => {
    
-          <img  src={PhotoThree}  className="object-contain "></img>
-    
-
+function handleClick(event){
+   console.log(event.target)
+}
+  return (
+    <> 
+    <PopUp></PopUp>
+      <div className="right_bottom_rectangle">
+        <div className="columns-1 gap-5 sm:columns-2 sm:gap-8 md:columns-3 lg:columns-3 [&>img:not(:first-child)]:mt-8 ">
+          {arts_data.map((art) => (
+            <img id = {art.id} src={art.img_link} onClick={handleClick}  className="object-contain"></img>
+          ))}
+        </div>
         
-         
-          <img  src={PhotoFour}  className="object-contain " ></img>
-      
-
-    
-          <img  src={PhotoFive}  className="object-contain " ></img>
-       
-
-         
-          <img  src={PhotoSix}  className="object-contain " ></img>
-    
-          </div>
-          
-          </div> 
-         
-
-       <div className="bottom_mask"></div>
-       </>
-    
-
-    )
+      </div>
+      <div className="bottom_mask"> </div>{" "}
+     
+    </>
+  );
 };
 
 export default Art;
