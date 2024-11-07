670821bc617464242f27f0c47cb2b24f62783353
Claudia00022 <klaudiaforysiak@gmail.com>
2024-11-07 18:11:24 +0000

    prettier

--- src/SmileFace.jsx
+++ src/SmileFace.jsx
@@ -1,14 +1,17 @@
 'use client'
-import React, {useRef} from "react";
-import { Canvas, useFrame, useLoader } from "@react-three/fiber";
+import React, {useLayoutEffect, useRef} from "react";
+import { Canvas, useFrame, useLoader, useThree } from "@react-three/fiber";
 import { TextureLoader } from "three";
 import { motion } from "framer-motion-3d";
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader'
 import { OrbitControls } from "@react-three/drei";
+import gsap from "gsap";
 
 
 export default function SmileFace(props) {
   const texture = useLoader(TextureLoader, "/img/earth.jpg.jpg");
+
+ 
 
   function Model(){
     const gltf = useLoader(GLTFLoader, './img/smileFace.glb');
@@ -17,8 +20,8 @@ export default function SmileFace(props) {
 
     useFrame(() => {
 
-        // ref.current.rotation.x  += 0.01;
-        ref.current.rotation.y  += 0.01;
+        // ref.current.rotation.x  += 0.001;
+        ref.current.rotation.y  += 0.001;
     
     })
 
@@ -35,6 +38,71 @@ export default function SmileFace(props) {
 
   }
 
+  function ModelTwo(){
+    const gltf = useLoader(GLTFLoader, './img/smileFace2.glb');
+    const ref = useRef(null);
+     
+
+    useFrame(() => {
+
+        // ref.current.rotation.x  += 0.01;
+        ref.current.rotation.y  += 0.001;
+    
+    })
+
+    return(
+      <mesh>
+      <primitive
+      ref = {ref}
+      object={gltf.scene}
+      position = {[10,-5,-5]}
+     
+      />
+      </mesh>
+    )
+
+  }
+
+  function ModelThree(){
+    const gltf = useLoader(GLTFLoader, './img/smileFace3.glb');
+    const ref = useRef(null);
+     
+
+    useFrame(() => {
+
+        // ref.current.rotation.x  += 0.001;
+        ref.current.rotation.y  += 0.001;
+    
+    })
+
+    return(
+      <mesh>
+      <primitive
+      ref = {ref}
+      object={gltf.scene}
+      position = {[-10,-5,-5]}
+     
+      />
+      </mesh>
+    )
+
+  }
+
+   function CamearaAnimation(){
+    const {camera} = useThree();
+
+    useLayoutEffect(() => {
+      gsap.to(camera.position, {
+        x: 10,
+        duration:50,
+        ease:'power2.inOut',
+        repeat: -1,
+        yoyo: true,
+      });
+    }, [camera]);
+    return null;   
+  }
+
   return (
     <>
       <div className="h-screen w-screen relative ">
@@ -44,10 +112,13 @@ export default function SmileFace(props) {
         
         // camera={{ position: [8, 5, 10] }}
           >
+          <CamearaAnimation />
             <ambientLight intensity={1.0} />
             <directionalLight intensity={2.5} position={[1, 5, 0]} />
-
             <Model  />
+            <ModelTwo />
+            <ModelThree />
+          
             {/* <OrbitControls target={[0, 1, 0]}  /> */}
           </Canvas>{" "}
         </div>{" "}
