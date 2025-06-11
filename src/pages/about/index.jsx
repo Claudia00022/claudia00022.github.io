88287e4bb4bdb44a3129ed09ac161695ed940403
Claudia00022 <klaudiaforysiak@gmail.com>
2025-06-11 11:03:31 +0100

    res

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -43,54 +43,62 @@ function About(props) {
 
   return (
     <>
-      <div
-        ref={props.about_section}
-        className=" relative xs:h-[150vh] md:h-full -z-10  "
-        style={{
-          backgroundColor: "#F4F2ED",
-          borderLeft: "5px solid #5F605F",
-          borderRight: "3px solid #5F605F",
-        }}
-      >
-        <div className="absolute   left-0 w-full bg-blue-200   ">
-          <div className=" xs:h-[150vh] flex xs:flex-col-reverse md:flex-row-reverse justify-around">
-            <div className="xs:w-full md:w-5/6 md:left-5 p-5   ">
-              <div className=" h-full flex xs:flex-col-reverse md:flex-row  md:justify-end md:items-end bg-red-200 xs:mt-10 md:mt-0 ">
-                <div className=" lg:w-1/4 text xs:text-xs mt-3 pb-5 text-black   bg-green-200   ">
-                  <p>
-                    I am <span className="italic">FREELANCE </span> frontend
-                    developer with a passion for creating dynamic and responsive
-                    web applications using the React framework.
-                    <br></br>I have honed my skills in building user-friendly
-                    interfaces and implementing best practices in web
-                    development.
-                    <br></br> My commitment to staying updated with the latest
-                    technologies ensures that I deliver modern and efficient
-                    solutions to my clients.With a passion for art, I thrive on
-                    collaborating with individuals and businesses that value
-                    aesthetics and recognize the power of simplicity.
-                  </p>
-                </div>
+     <div
+  ref={props.about_section}
+  className="relative min-h-screen -z-10 overflow-y-auto"
+  style={{
+    backgroundColor: "#F4F2ED",
+    borderLeft: "5px solid #5F605F",
+    borderRight: "3px solid #5F605F",
+  }}
+>
+  <div className="w-full">
+    <div className="flex flex-col-reverse lg:flex-row-reverse justify-around">
+      <div className="w-full lg:w-5/6 p-10">
+        <div className="flex flex-col-reverse lg:flex-row justify-around lg:justify-between lg:items-end mt-10 lg:mt-0 gap-4  ">
+          {/* TEKST */}
+          <div className="w-full lg:w-5/12 text-xs md:text-sm lg:text-lg text-black mt-3 lg:pe-10 lg:ps-10 overflow-y-auto max-h-[60vh] ">
+            <p className="text opacity-100 tracking-tighter">
+              ****I am <span className="italic">FREELANCE </span> frontend developer
+              with a passion for creating dynamic and responsive web applications
+              using the React framework.
+              <br />
+              I have honed my skills in building user-friendly interfaces and
+              implementing best practices in web development.
+              <br />
+              My commitment to staying updated with the latest technologies ensures
+              that I deliver modern and efficient solutions to my clients. With a
+              passion for art, I thrive on collaborating with individuals and
+              businesses that value aesthetics and recognize the power of simplicity.
+            </p>
+          </div>
 
-                <div className=" relative xs:w-full md:w-1/2 lg:mt-5 overflow-hidden">
-                  <img src={Ja} alt="myImage" className="w-full h-auto object-cover"></img>
-                  <Smile smile_container={props.smile_container} />
-                </div>
-              </div>
-            </div>
-
-            <div className="xs:w-full md:w-3/6 md:mt-10 xs:ps-5 md:pb-5 md:pt-5   bg-slate-50 self-start">
-              <p
-                className="text xs:text-base xl:text-2xl"
-                style={{ opacity: 1, color: "#3F3B37" }}
-              >
-                01/
-              </p>
-              <AnimatedText />
-            </div>
+          {/* OBRAZEK */}
+          <div className="relative w-full lg:w-1/2 lg:mt-5 overflow-hidden">
+            <img
+              src={Ja}
+              alt="myImage"
+              className="w-full h-auto object-cover"
+            />
+            <Smile smile_container={props.smile_container} />
           </div>
         </div>
       </div>
+
+      {/* SEKCJA 01 + TEKST ANIMOWANY */}
+      <div className="w-full lg:w-3/6 mt-10 ps-10 md:pb-5 md:pt-5 bg-slate-50  self-start">
+        <p
+          className="text-base xl:text-2xl"
+          style={{ opacity: 1, color: "#3F3B37" }}
+        >
+          01/
+        </p>
+        <AnimatedText />
+      </div>
+    </div>
+  </div>
+</div>
+
     </>
   );
 }
